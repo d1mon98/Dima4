b1ee1e9497dba675abc38f55b16038bf3c1127b0
d1mon98 <93488624+d1mon98@users.noreply.github.com>
2022-11-06 01:51:06 +0600

    lesson 2 homework

--- src/main/java/zaur/lesson2/second.java
+++ src/main/java/zaur/lesson2/second.java
@@ -2,13 +2,75 @@ package zaur.lesson2;
 
 public class second {
     public static void main(String[] args) {
+
+        byte a1 = 12;               //decimal
+        byte a2 = 0_14;             //octal
+        byte a3 = 0xC;              //hexadecimal
+        byte a4 = 0b1100;           //binary
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
         System.out.println();
-        System.out.println("РУБАИ");
-        System.out.println("Много лет размышлял я над жизнью земной.");
-        System.out.println("Непонятного нет для меня под луной.");
-        System.out.println("Мне известно, что мне ничего не известно!");
-        System.out.println("Вот последняя правда, открытая мной.");
-        System.out.println("О. Хайям");
+        short b1 = -1300;                    //decimal
+        short b2 = -0_2424;                  //octal
+        short b3 = -0x514;                   //hecadecimal
+        short b4 = -0b0101_0001_0100;        //binary
+
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
         System.out.println();
+        int c1=0;
+        int c2=0_0;
+        int c3=0x0;
+        int c4=0b0;
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+        System.out.println();
+
+        long d1 = 123456789L;
+        long d2 = 0_726746425L;
+        long d3 = 0x75BCD15L;
+        long d4 = 0b0111_0101_1011_1100_1101_0001_0101L;
+
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+        System.out.println();
+
+        float e1 = 4.5F;
+        float e2 = 5666.444F;
+        double e3 = 44.3;
+        double e4 = -0.4343;
+        boolean e5 = true;
+        boolean e6 = false;
+
+        System.out.println(e1);
+        System.out.println(e2);
+        System.out.println(e3);
+        System.out.println(e4);
+        System.out.println(e5);
+        System.out.println(e6);
+        System.out.println();
+
+        char f1 = '!';
+        char f2 =  33;
+        char f3 = '\u0021';
+
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
+
+
+        }
     }
-}
+
+
