d93ebf3e2aa322b7bca54e78a286c4c08ab6746a
d1mon98 <93488624+d1mon98@users.noreply.github.com>
2022-11-06 17:33:07 +0600

    lesson 2 homework

--- src/main/java/zaur/lesson1/first.java
+++ src/main/java/zaur/lesson1/first.java
@@ -1,11 +1,14 @@
 package zaur.lesson1;
 
 public class first {
-
     public static void main(String[] args) {
-        System.out.println("Загрузка успешно завершена");
-        System.out.print("sss ");
-        System.out.print("ssssss");
+        System.out.println();
+        System.out.println("               РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                                  О. Хайям");
+        System.out.println();
     }
-
 }
